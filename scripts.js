59349aae9b86fd4ea49a2603d18c72714729f2bf
bongo-animal <nyl.julioduran@gmail.com>
2022-01-21 09:57:58 -0600

    ejercicios

---
+++ scripts.js
@@ -0,0 +1,37 @@
+/*
+fetch(URL)
+.then(convertir a formato JSON)
+.then(instrucciones de lo que se quiere hacer mientras se cumpla la promesa)
+
+.catch(en caso contrario mandar error)
+
+*/
+
+/*
+
+function puntoUno(){
+  //document.getElementById("container").style.backgroundColor = "red"
+
+  //document.querySelector("#container").style.backgroundColor = "red";  
+
+  document.querySelectorAll(".second");
+
+
+}*/
+/*
+function puntoUno(){
+  var x;
+  x = document.querySelectorAll(".second");
+  
+      x[0].style.backgroundColor = "blue";
+      x[1].style.backgroundColor = "blue";
+  }*/
+
+
+  function puntoUno(){
+    var x;
+    x = document.querySelectorAll(".third");
+    
+        x[1].style.backgroundColor = "blue";
+    }
+  
